7775e5f87d2d671459ab9b41052da8a5d5bdf057
Tobi Tselios <tobi.tselios@gmail.com>
2020-05-18 18:00:58 +0100

    Added lightweight generics andimplemented the property getter and instance variable.

--- Contacts-Swift-Hybrid/Contacts-Hybrid/OTKContactController.m
+++ Contacts-Swift-Hybrid/Contacts-Hybrid/OTKContactController.m
@@ -10,4 +10,22 @@
 
 @implementation OTKContactController
 
+@synthesize contacts = _contacts;
+- (NSArray<Contact *> *)contacts {
+    if (!_contacts) {
+        _contacts = [[NSArray alloc] init];
+    }
+
+    return _contacts;
+}
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+
+    }
+
+    return self;
+}
+
 @end
