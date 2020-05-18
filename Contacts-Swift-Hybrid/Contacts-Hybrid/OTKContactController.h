84eddd2ef3654128837192acb94812c0ece14674
Tobi Tselios <tobi.tselios@gmail.com>
2020-05-18 17:33:52 +0100

    Added Contact Controller, bridging header and organised project

---
+++ Contacts-Swift-Hybrid/Contacts-Hybrid/OTKContactController.h
@@ -0,0 +1,17 @@
+//
+//  OTKContactController.h
+//  Contacts-Hybrid
+//
+//  Created by Tobi Kuyoro on 18/05/2020.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OTKContactController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Contacts-Swift-Hybrid/Contacts-Hybrid/OTKContactController.m
@@ -0,0 +1,13 @@
+//
+//  OTKContactController.m
+//  Contacts-Hybrid
+//
+//  Created by Tobi Kuyoro on 18/05/2020.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "OTKContactController.h"
+
+@implementation OTKContactController
+
+@end
